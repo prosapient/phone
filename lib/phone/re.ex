dca67b8fd2591907cde2af1bbf17cd65d9c5848a
VovaGula <gula.volodya@gmail.com>
2021-05-07 12:41:07 +0300

    fix(parser): phone parser for Reunion country
    We had wrong alpha3 for the "Reunion" country.
    This should be "REU"(was "RET").
    See ISO: https://www.iso.org/obp/ui/#iso:code:3166:RE

--- lib/phone/re.ex
+++ lib/phone/re.ex
@@ -6,7 +6,7 @@ defmodule Phone.RE do
   def regex, do: ~r/^(262)()(.{9})/
   def country, do: "Reunion"
   def a2, do: "RE"
-  def a3, do: "RET"
+  def a3, do: "REU"
 
   matcher(:regex, ["262"])
 end
